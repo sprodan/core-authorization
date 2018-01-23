54fcfc6280458d20d0654b87362251e7a3fa811a
Продан Станислав Анатолиевич <sprodan8@gmail.com>
2018-01-23 17:05:18 +0330

    Users adding

--- Authorization/Pages/Settings/Users/Add.cshtml.cs
+++ Authorization/Pages/Settings/Users/Add.cshtml.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
 using Authorization.Data;
+using Authorization.Extentions;
 using Authorization.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Primitives;
 
 namespace Authorization.Pages.Settings.Users
 {
@@ -17,18 +20,57 @@ namespace Authorization.Pages.Settings.Users
             this.Title = "Добавить пользователя";
             this.Breadcrumbs = new Queue<Breadcrumb>();
             Breadcrumbs.Enqueue(new Breadcrumb { Title = "Главная", NavigationUrl = "/" });
-            Breadcrumbs.Enqueue(new Breadcrumb { Title = "Настройки" });
+            Breadcrumbs.Enqueue(new Breadcrumb { Title = "..." });
             Breadcrumbs.Enqueue(new Breadcrumb { Title = "Пользователи", NavigationUrl = "/settings/users/index" });
+            Breadcrumbs.Enqueue(new Breadcrumb { Title = "Добавить", NavigationUrl = "/settings/users/add" });
+
         }
 
         [BindProperty]
         public List<Employee> Employees { get; set; }
+        [BindProperty]
+        public List<Role> Roles { get; set; }
 
         public async Task<IActionResult> OnGetAsync()
         {
             if (!base.CheckPermitions(this.Request.Headers)) return Redirect("/error");
             Employees = await _db.Employees.ToListAsync();
+            Roles = await _db.Roles.ToListAsync();
             return Page();
+        }
+        [AjaxOnly]
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!base.CheckPermitions(this.Request.Headers)) return Redirect("/error");
+            if (Request.Form.TryGetValue("jsonRequest", out StringValues data))
+            {
+                var dict = data.ToString().DeserializeAjaxString();
+                var login = HttpUtility.UrlDecode(dict["login"]);
+                var password = HttpUtility.UrlDecode(dict["password"]);
+                var roleId = HttpUtility.UrlDecode(dict["roleId"]);
+                var employeeId = HttpUtility.UrlDecode(dict["employeeId"]);
+
+                if (!string.IsNullOrWhiteSpace(login) && 
+                    !string.IsNullOrWhiteSpace(password) &&
+                    int.TryParse(roleId, out int iroleId) &&
+                    int.TryParse(employeeId, out int iemployeeId))
+                {
+                    var role = await _db.Roles.FindAsync(iroleId);
+                    var employee = await _db.Employees.FindAsync(iemployeeId);
+                    var user = await _db.Users.AddAsync(
+                        new Data.User()
+                        {
+                            Login = login,
+                            IsActive = true,
+                            Password = password,
+                            Role = role,
+                            Employee = employee
+                        });
+                    await _db.SaveChangesAsync();
+                    return new JsonResult(new { Status = "OK", Code = 200, User = user.Entity });
+                }
+            }
+            return new JsonResult(new { Status = "ERROR", Code = 500 });
         }
     }
 }
--- Authorization/Pages/Settings/Users/Index.cshtml.cs
+++ Authorization/Pages/Settings/Users/Index.cshtml.cs
@@ -8,6 +8,9 @@ using Authorization.Models;
 using Authorization.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http;
+using Authorization.Extentions;
+using System.Web;
+using Microsoft.Extensions.Primitives;
 
 namespace Authorization.Pages.Settings.Users
 {
@@ -20,6 +23,9 @@ namespace Authorization.Pages.Settings.Users
             Breadcrumbs.Enqueue(new Breadcrumb { Title = "Главная", NavigationUrl = "/" });
             Breadcrumbs.Enqueue(new Breadcrumb { Title = "Настройки" });
             Breadcrumbs.Enqueue(new Breadcrumb { Title = "Пользователи", NavigationUrl = "/settings/users/index" });
+            this.ActionCode = "221";
+            this.ActionTitle = "Добавить";
+            this.ActionUrl = "/settings/users/add";
         }
         [BindProperty]
         public List<Models.User> Users { get; set; }
@@ -34,12 +40,11 @@ namespace Authorization.Pages.Settings.Users
             foreach(var user in users)
             {
                 var isOnline = false;
-                var role = await _db.Roles.FindAsync(user.Role.Id);
+                if (!user.IsActive) continue;
                 var userModel = new Models.User()
                 {
                     Id = user.Id,
                     Login = user.Login,
-                    Role = role,
                     IsOnline = isOnline //TODO: calculateOnline
                 };
                 if (user.Employee != null)
@@ -47,9 +52,61 @@ namespace Authorization.Pages.Settings.Users
                     var employee = employees.Find(x => x.Id == user.Employee.Id);
                     userModel.Name = $"{employee?.Name} {employee?.Surname}";
                 }
+                if (user.Role != null)
+                {
+                    var role = await _db.Roles.FindAsync(user.Role.Id);
+                    userModel.Role = role;
+                }
                 Users.Add(userModel);
             }
             return Page();
+        }
+
+        [AjaxOnly]
+        public async Task<IActionResult> OnPostBlockUserAsync()
+        {
+            if (!base.CheckPermitions(this.Request.Headers)) return Redirect("/error");
+            if (Request.Form.TryGetValue("jsonRequest", out StringValues data))
+            {
+                var dict = data.ToString().DeserializeAjaxString();
+                var id = HttpUtility.UrlDecode(dict["id_user"]);
+
+                if (int.TryParse(id, out var idUser))
+                {
+                    var user = await _db.Users.FindAsync(idUser);
+                    if(user != null)
+                    {
+                        user.IsActive = false;
+                    }
+                    await _db.SaveChangesAsync();
+                    return new JsonResult(new { Status = "OK", Code = 200, User = user });
+                }
+            }
+            return new JsonResult(new { Status = "ERROR", Code = 500 });
+        }
+        [AjaxOnly]
+        public async Task<IActionResult> OnPostEditRoleAsync()
+        {
+            if (!base.CheckPermitions(this.Request.Headers)) return Redirect("/error");
+            if (Request.Form.TryGetValue("jsonRequest", out StringValues data))
+            {
+                var dict = data.ToString().DeserializeAjaxString();
+                var idu = HttpUtility.UrlDecode(dict["id_user"]);
+                var idr = HttpUtility.UrlDecode(dict["id_role"]);
+                if (int.TryParse(idu, out var idUser) &&
+                    int.TryParse(idr, out var idRole))
+                {
+                    var user = await _db.Users.FindAsync(idUser);
+                    var role = await _db.Roles.FindAsync(idRole);
+                    if (user != null)
+                    {
+                        user.Role = role;
+                    }
+                    await _db.SaveChangesAsync();
+                    return new JsonResult(new { Status = "OK", Code = 200, User = user });
+                }
+            }
+            return new JsonResult(new { Status = "ERROR", Code = 500 });
         }
     }
 }
